e920cef37213c4dcb186303ead860eb59a4be6c4
Kris <kristian.maras@sydney.edu.au>
2024-01-24 11:10:43 +1100

    trying bulk ingestion of signal info

--- feature_engineering.R
+++ feature_engineering.R
@@ -2,7 +2,7 @@ library(here)
 library(tidyverse)
 library(readxl) # excel files
 library(xml2) #xml files
-
+library(plotly)
 labelled_data_path <- here::here("data","S18","labelled")
 
 main_data_path <- here::here("data","S18","Export_Analysis-01_05_2024-17-02-34")
@@ -29,26 +29,83 @@ rm(LVpLabelledData,RVpLabelledData,SRLabelledData)
 
 
 find_window <- function(WaveFront, Catheter_Type, Point_Number) {
-  #Find the window of interest in the signal as specified by Point Export files.
-  # For example: 9-LV SR Penta_P4815_Point_Export.xml
-  # find_window("SR","Penta",4815) and find_window("RVp","Penta",4815) has files
+  # Extracts key elements from the Point Export Files.
+  # Find the window of interest in the signal as specified by Point Export files.
+  # Example: find_window("SR","Penta",4815) and find_window("RVp","Penta",4815) has files and windows of interest
   # find_window("LVp","Penta",4815) does not exist and NULL returned
 
   file_pattern <- paste0(".*", WaveFront, "\\s", Catheter_Type, "_P", Point_Number, "_Point_Export\\.xml$")
-  matching_files <- list.files(path = main_data_path,
+  matching_file <- list.files(path = main_data_path,
                                pattern = file_pattern, full.names = TRUE)
+  #expect one file
+  if (length(matching_file) == 1 ) {
+    xml_data <- read_xml(matching_file)
+    # Define XPath expressions to extract key elements from xml file
+    reference_annotation <- xml_find_all(xml_data, "//Annotations/@Reference_Annotation") %>%
+      xml_text() %>% as.numeric()
+    woi_from <- xml_find_all(xml_data, "//WOI/@From") %>%
+      xml_text() %>% as.numeric()
+    woi_to <- xml_find_all(xml_data, "//WOI/@To") %>%
+      xml_text() %>% as.numeric()
 
-  if (length(matching_files) > 0 ) {
-    return(matching_files)
+    woi <- list(From = reference_annotation + woi_from, To = reference_annotation + woi_to)
+    return(woi)
   } else {
     return() # returns NULL and can use is.null() to check
   }
 
 }
 
-# to be continued..
-lookslike <- read_xml(find_window("SR","Penta",4815))
+find_signal_file <- function(WaveFront, Catheter_Type, Point_Number) {
+  #Given you have a woi, get signal information
+  pattern <- sprintf(".*%s %s_P%s_ECG_Export\\.txt$", WaveFront, Catheter_Type, Point_Number)
+
+  print(pattern)
+  print(main_data_path)
+  matching_file <- list.files(path = main_data_path,
+                              pattern = pattern, full.names = TRUE)
 
 
+  return(matching_file)
+
+}
 
 
+get_signal_data <- function(WaveFront, Catheter_Type, Point_Number) {
+  # read the signal information
+  # Assumes rows where the data starts is constant.
+
+  woi <- find_window(WaveFront,Catheter_Type,Point_Number)
+  txt_file <- find_signal_file(WaveFront,Catheter_Type,Point_Number)
+  tabular_content <-  read_table(txt_file, skip = 3)
+  raw_ecg_gain <- str_extract(read_lines(txt_file,n_max = 4), "^Raw ECG to MV \\(gain\\) = ([0-9.]+)$") %>%
+    parse_number()
+  raw_ecg_gain <- raw_ecg_gain[!is.na(raw_ecg_gain)]
+
+  #double check gain and channels are extracted from txt file
+
+  if (!is.numeric(raw_ecg_gain)) {
+    return() #return null
+  }
+
+  if (is.null(channel)) {
+    return() #return null
+  }
+
+  channel <- str_match(read_lines(txt_file,n_max = 4), "Bipolar Mapping Channel=(\\w+-\\w+)")[3 , 2]
+  #find appropriate column to extract bipolar recordings given channel and window of interest
+
+  signal <- tabular_content %>% select(matches(channel)) %>%
+            slice(.,(woi$From + 5):(woi$To +5) ) * raw_ecg_gain
+
+  return(signal)
+}
+
+retrieve_all_signals <- function() {
+  LabelledSignalData <- LabelledData %>% rowwise  %>% mutate(signal = list(get_signal_data(WaveFront, Catheter_Type, Point_Number)))
+}
+
+# this confirms the graph in the data dictionary
+data <- get_signal_data("SR","Penta",4815) %>% as_tibble()
+plot <- plot_ly(data, y = ~`20A_17-18(129)`, type = 'scatter', mode = 'lines')
+plot
