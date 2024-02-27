af4f447bdb5c87197cd557aaa350820bdf1b38c3
Seb Haan <sebhaan@gmail.com>
2024-02-27 14:09:50 +1100

    Add docs and cleanup code

--- python/features.py
+++ python/features.py
@@ -10,14 +10,6 @@ from tsfresh import feature_extraction
 from tsfresh.utilities.dataframe_functions import impute
 from tsfresh.feature_extraction import feature_calculators
 
-## Settings
-
-inpath = '../../../data/generated'
-fname_csv = 'NestedDataS18.csv'
-wavefront = 'SR' # select from: LVp, RVp, or SR
-target = 'scar' # select from: 'scar', 'endocardium_scar', 'intramural_scar', 'epicardial_scar'
-outpath = '../../../data/features'
-
 
 class FeatureExtraction:
     """
@@ -25,16 +17,21 @@ class FeatureExtraction:
 
     This class extracts features from a timeseries and selects relevant features based on the tsfresh library.
 
-    The algorithm includes two steps:
+    The algorithm includes three main steps:
+    
     1) Feature extraction: the algorithm characterizes time series with comprehensive and well-established feature mappings 
     and considers additional features describing meta-information.
+    
     2) Feature significance testing: each feature vector is individually and independently evaluated 
     with respect to its significance for predicting the target under investigation.
+    
     3) Feature selection: The vector of p-values is evaluated on the basis of the Benjamini-Yekutieli procedure
     in order to decide which features to keep. 
 
     Tsfresh deploys the fresh algorithm (fresh stands for FeatuRe Extraction based on Scalable Hypothesis tests).
-    http://adsabs.harvard.edu/abs/2016arXiv161007717C
+    References:
+        - http://adsabs.harvard.edu/abs/2016arXiv161007717C
+        - https://tsfresh.readthedocs.io/en/latest/index.html
 
     Args:
         inpath (str): Path to the input csv file
@@ -57,7 +54,6 @@ class FeatureExtraction:
         self.wavefront = None
         os.makedirs(self.outpath, exist_ok=True)
         self.load_data()
-
 
     def load_data(self):
         """ Load the data from the csv file and pprepare the dataframe."""
@@ -98,7 +94,6 @@ class FeatureExtraction:
         self.y = self.df[self.df['WaveFront'] == self.wavefront][['Point_Number', self.target]].drop_duplicates()
         # set y to  pandas.Series
         self.y = self.y.set_index('Point_Number')[self.target]
-
 
     def extract_features(self):
         """ Extract features from the timeseries"""
