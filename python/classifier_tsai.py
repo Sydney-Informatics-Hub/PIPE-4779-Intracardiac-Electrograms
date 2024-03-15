f804c19d9be24f62e3820012ac67da1ee02b2a7a
Seb Haan <sebhaan@gmail.com>
2024-03-15 17:42:11 +1100

    Add load and formating data

--- python/classifier_tsai.py
+++ python/classifier_tsai.py
@@ -9,10 +9,60 @@ import numpy as np
 import pandas as pd
 
 inpath = '../../../data/generated'
-fname = 'NestedDataS18.csv'
+fname_csv = 'NestedDataS18.csv'
+
+def load_data(inpath, fname_csv):
+    usecols = [
+        'Point_Number',
+        'WaveFront',
+        'sheep',
+        'signal_data',
+        'endocardium_scar',
+        'intramural_scar',
+        'epicardial_scar',
+    ]
+    # check if file exists
+    if not os.path.isfile(os.path.join(inpath, fname_csv)):
+        raise FileNotFoundError(f'File {fname_csv} not found in {inpath}')
+    df = pd.read_csv(os.path.join(inpath, fname_csv), usecols=usecols)
+    # remove nan values
+    df = df.dropna()
+    # add column "time" to df which starts at 0 and has the same length as "signal_data" for each Point_Number and WaveFront
+    df['time'] =df.groupby(['Point_Number', 'WaveFront']).cumcount()
+    # Generate a column 'scar' that is 1 if either of the scar columns is 1, otherwise 0
+    df['endocardium_scar'] = df['endocardium_scar'].astype(int)
+    df['intramural_scar'] = df['intramural_scar'].astype(int)
+    df['epicardial_scar'] =df['epicardial_scar'].astype(int)
+    df['scar'] = df[['endocardium_scar', 'intramural_scar', 'epicardial_scar']].max(axis=1)
+
+def df_to_ts(df, wavefront, target='scar'):
+    """
+    Converts the dataframe to a timeseries for the given wavefront
+
+    Args:
+        wavefront (str): 'LVp', 'RVp', or 'SR'
+        target (str): 'scar' (Default) or 'endocardium_scar', 'intramural_scar', 'epicardial_scar'
+    """
+    dfsel = df[df['WaveFront'] == wavefront][['Point_Number', 'time', 'signal_data', target]]
+    npoints_unique = dfsel['Point_Number'].nunique()
+    signal = [] #np.zeros((npoints_unique, timeseries['signal_data'].apply(len).max()))
+    y = dfsel[['Point_Number', target]].drop_duplicates()
+    # get length of signal_data for each point
+    signal_length = dfsel.groupby('Point_Number')['signal_data'].apply(len)
+    X = np.zeros((len(y), signal_length.max()))
+    for i in range(len(y)):
+        point = y.iloc[i]['Point_Number']
+        # get signal_data
+        data = dfsel[dfsel['Point_Number'] == point]['signal_data']
+        X[i, :len(data)] = data
+
+    return X.reshape((len(y), 1, -1)), y[target].values
 
 # load data
 df = pd.read_csv(os.path.join(inpath, fname))
+
+wavefront = 'SR'
+target = 'scar'
 
 
 # split data
