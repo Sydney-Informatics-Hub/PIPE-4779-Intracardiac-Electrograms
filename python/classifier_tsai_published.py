a259ff7b76fa753f1aef9d74751d14288b99795f
Nathaniel Butterworth <natbutter@gmail.com>
2024-04-26 15:16:13 +1000

    modified df_to_ts

--- python/classifier_tsai_published.py
+++ python/classifier_tsai_published.py
@@ -1,11 +1,11 @@
 from classifier_tsai import run_all
 
 
-def main():
-    inpath = '../data/generated/'
+if __name__ == '__main__':
+    inpath = '../DATA/generated/'
     fname_csv = 'publishable_model_data_TSAI.parquet'
     outpath = '../deploy/output'
-    target_list = ['NoScar', 'AtLeastEndo', 'AtLeastIntra', 'epiOnly'] 
+    target_list = ['NoScar', 'AtLeastEndo', 'AtLeastIntra', 'epiOnly']
     run_all(inpath ,fname_csv,outpath,target_list)
     #tsai = TSai(inpath, fname_csv)
     #df = tsai.load_data(inpath, fname_csv)
