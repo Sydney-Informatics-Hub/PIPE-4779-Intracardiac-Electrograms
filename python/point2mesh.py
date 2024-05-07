09e71cac3b5081b09d7cdf074d1d45b03fd63850
Seb Haan <sebhaan@gmail.com>
2024-05-07 14:59:01 +1000

    add run function and cleanup code

--- python/point2mesh.py
+++ python/point2mesh.py
@@ -16,7 +16,18 @@ import pandas as pd
 from carto_reader.carto_data import Carto
 
 class MeshDataMapper:
-    def __init__(self, path_data_export. mesh_file, point_data_file):
+    """
+    Interpolates point data onto a mesh geometry using a Gaussian kernel.
+
+    Args:
+        path_data_export: Path to the data export directory containing the mesh file.
+        mesh_file: Path and filename to the the mesh file.
+        point_data_file: Path and filename to the point data file (parquet) with X, Y, Z, and data columns (prediction, probability).
+        fname_out: Path and filename to the output file to save the mesh with mapped data.
+        vtk_meta_text: Text to add to the second line of the VTK file.
+    
+    """
+    def __init__(self, path_data_export. mesh_file, point_data_file, fname_out, vtk_meta_text=""):
         self.mesh_file = mesh_file
         self.point_data_file = point_data_file
         self.path_data_export = path_data_export
@@ -28,6 +39,8 @@ class MeshDataMapper:
         self.df = None
         self.predictions = None
         self.probabilities = None
+        self.fname_out = fname_out
+        self.vtk_meta_text = vtk_meta_text
 
     def read_carto(self):
         """Reads a mesh file."""
@@ -53,13 +66,12 @@ class MeshDataMapper:
         self.predictions = self.predictions.astype(int)
         self.probabilities = self.df['probability'].values
 
-    def map_point_data_onto_mesh(self, vtk_meta_txt, null_strategy='closest_point'):
+    def map_point_data_onto_mesh(self, null_strategy='closest_point'):
         """
         Interpolates point data onto the mesh.
         This uses a Gaussian interpolation kernel (using default kernel: sharpness =2)
 
         Args:
-            vtk_meta_txt: string with metadata to write to the vtk file
             null_strategy: Specify a strategy to use when encountering a “null” point during the interpolation process.
                 Default: 'closest_point'
                 Options: 'mask_points', 'null_value', 'closest_point' 
@@ -75,19 +87,19 @@ class MeshDataMapper:
         # interpolate point data onto mesh
         self.mesh_ipl = mesh_pv.interpolate(points, strategy=null_strategy)
 
-    def write_mesh_with_data(self, output_file, vtk_meta_txt):
+    def write_mesh_with_data(self):
         """Writes the mesh with the mapped point data to a file."""
         if self.mesh_ipl is None:
             raise ValueError("No interpolated mesh available to write.")
         # save mesh with mapped data as vtk
-        self.mesh_ipl.save(self.path_out, binary=False)
+        self.mesh_ipl.save(self.fname_out, binary=False)
         # replace second line of vtk file with txt string :
-        with open(self.path_out, 'r') as file:
+        with open(self.fname_out, 'r') as file:
             lines = file.readlines()
-        lines[1] = vtk_meta_txt + "\n"
-        with open(self.path_out, 'w') as file:
+        lines[1] = self.vtk_meta_text + "\n"
+        with open(self.fname_out, 'w') as file:
             file.writelines(lines)
-        print(f"Mesh with mapped data saved to {self.path_out}")
+        print(f"Mesh with mapped data saved to {self.fname_out}")
 
     def plot_mesh_and_points(mesh, add_points=True):
         # plot mesh with mapped data
@@ -97,16 +109,22 @@ class MeshDataMapper:
             p.add_points(self.df[['X', 'Y', 'Z']].values, scalars=self.predictions, cmap='viridis')
         p.show()
 
+    def run(self, null_strategy='closest_point'):
+        self.read_carto()
+        self.read_point_data()
+        self.map_point_data_onto_mesh(null_strategy)
+        self.write_mesh_with_data()
+
 
 def test_MeshDataMapper():
     path_data_export = '../../../data/deploy/data/Export_Analysis'
     mesh_file= '../../../data/deploy/data/Export_Analysis/9-LV SR Penta.mesh'
     point_data_file = '../../../data/deploy/data/predictions.parquet'
-    path_out = '../../../data/deploy/data/predictions_mapped.vtk'
+    fname_out = '../../../data/deploy/data/predictions_mapped.vtk'
     vtk_meta_text = "PatientData S18 S18 4290_S18”
 
     mapper = MeshDataMapper(path_data_export, mesh_file, point_data_file)
     mapper.read_carto()
     mapper.read_point_data()
     mapper.map_point_data_onto_mesh()
-    mapper.write_mesh_with_data(path_out, vtk_meta_text)
+    mapper.write_mesh_with_data()
