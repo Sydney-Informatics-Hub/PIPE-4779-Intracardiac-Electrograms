9e9c42d94266cb0dccc99ae229c4879cde58bc25
Seb Haan <sebhaan@gmail.com>
2024-05-07 14:48:39 +1000

    Add plot function and docs

--- python/point2mesh.py
+++ python/point2mesh.py
@@ -1,10 +1,10 @@
 """
-Map point data onto mesh.
+Interpolate point data onto mesh geometry
 
 Functionality:
-- read in vtk file
-- read in point data file (parquet) with X, Y, Z, and data columns (predictions)
-- map point data onto mesh
+- read in carto mesh file
+- read in point data file (parquet) with X, Y, Z, and data columns (prediction, probability)
+- interpolate point data onto mesh using Gaussian kernel
 - write out vtk file with mapped point data
 
 need pip install carto-reader
@@ -37,8 +37,8 @@ class MeshDataMapper:
         mesh_name = os.path.basename(self.mesh_file).split('.')[0]
         idx = [i for i, s in enumerate(carto) if mesh_name in s][0]
         self.mesh = carto[idx].mesh
-        self.points_carto = carto[mesh_name].points
-        self.points_number = list(self.points_carto.keys())
+        points_carto = carto[mesh_name].points
+        self.points_number = list(points_carto.keys())
         #print("Mesh loaded with", self.mesh.n_points, "points and", self.mesh.n_cells, "cells.")
 
     def read_point_data(self):
@@ -53,8 +53,17 @@ class MeshDataMapper:
         self.predictions = self.predictions.astype(int)
         self.probabilities = self.df['probability'].values
 
-    def map_point_data_onto_mesh(self, vtk_meta_txt, intp_strategy='closest_point'):
-        """Maps point data onto the mesh."""
+    def map_point_data_onto_mesh(self, vtk_meta_txt, null_strategy='closest_point'):
+        """
+        Interpolates point data onto the mesh.
+        This uses a Gaussian interpolation kernel (using default kernel: sharpness =2)
+
+        Args:
+            vtk_meta_txt: string with metadata to write to the vtk file
+            null_strategy: Specify a strategy to use when encountering a “null” point during the interpolation process.
+                Default: 'closest_point'
+                Options: 'mask_points', 'null_value', 'closest_point' 
+        """
          # convert to pyvista mesh
         if self.mesh is None:
             raise ValueError("No mesh available to write.")
@@ -62,8 +71,9 @@ class MeshDataMapper:
          # Map the data from the point cloud to the mesh
         points = pv.PolyData(self.df[['X', 'Y', 'Z']].values)
         points['values'] = self.predictions
+        points['probabilities'] = self.probabilities
         # interpolate point data onto mesh
-        self.mesh_ipl = mesh_pv.interpolate(points,  strategy=intp_strategy)
+        self.mesh_ipl = mesh_pv.interpolate(points, strategy=null_strategy)
 
     def write_mesh_with_data(self, output_file, vtk_meta_txt):
         """Writes the mesh with the mapped point data to a file."""
@@ -79,11 +89,13 @@ class MeshDataMapper:
             file.writelines(lines)
         print(f"Mesh with mapped data saved to {self.path_out}")
 
-    def plot_mesh(mesh):
+    def plot_mesh_and_points(mesh, add_points=True):
         # plot mesh with mapped data
-        plot = mesh_ipl.plot(scalars='values', cmap='viridis')
-        # close plot
-        plot.close()
+        p = pv.Plotter()
+        p.add_mesh(mesh, scalars='values', cmap='viridis')
+        if add_points:
+            p.add_points(self.df[['X', 'Y', 'Z']].values, scalars=self.predictions, cmap='viridis')
+        p.show()
 
 
 def test_MeshDataMapper():
@@ -98,5 +110,3 @@ def test_MeshDataMapper():
     mapper.read_point_data()
     mapper.map_point_data_onto_mesh()
     mapper.write_mesh_with_data(path_out, vtk_meta_text)
-
-
