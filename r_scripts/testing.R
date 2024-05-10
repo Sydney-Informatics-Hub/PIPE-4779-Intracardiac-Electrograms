a5307e79bcfaf16ee1c83ea71555b4ecbc538a92
Kris <kristian.maras@sydney.edu.au>
2024-05-10 16:20:57 +1000

    python and r aggregations officially the same

---
+++ r_scripts/testing.R
@@ -0,0 +1,30 @@
+library(here)
+library(arrow)
+library(tidyverse)
+
+deploy_data_path <- here::here("deploy","data")
+
+df_python <- read_parquet(here(deploy_data_path,"preprocessed_rawsignal_unipolar_penta.parquet")) %>%
+  arrange(WaveFront,Point_Number) %>% mutate(WaveFront = as_factor(WaveFront)) %>% as_tibble()
+
+high_level <- df_python %>% unnest(signal) %>% group_by(WaveFront) %>% summarise(total = sum(signal))
+
+df_r <- read_parquet(here(deploy_data_path,"data_injest_fromR.parquet")) %>%
+        mutate(WaveFront = as_factor(WaveFront)) %>% as_tibble() %>%
+        arrange(WaveFront,Point_Number)
+
+
+
+high_level2 <-df_r %>% unnest(signal_data) %>% group_by(WaveFront) %>% summarise(total = sum(signal_data))
+
+high_level == high_level2 #true
+
+
+df_r_long <- df_r %>% unnest(signal_data)
+
+df_python_long <- df_python %>% rename(signal_data = signal ) %>% unnest(signal_data) %>%
+  rename(paths = files)
+
+result <- all.equal(df_python_long, df_r_long) #TRUE
+
+print(result == TRUE)#TRUE
