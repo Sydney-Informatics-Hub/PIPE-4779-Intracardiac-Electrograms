1e6deb2fbeee59433201668203bfbc3c026f3c87
Seb Haan <sebhaan@gmail.com>
2024-05-14 14:56:30 +1000

    add function for running all process stages

--- python/inference_pipe.py
+++ python/inference_pipe.py
@@ -17,12 +17,29 @@ from classifier_tsai import TSai
 # Settings
 raw_data_path = "../../../data/deploy/data/Export_Analysis"
 export_analysis_path =  "../../../data/deploy/data"
-fname_data = 'data.csv'
-path_model = 'models'
+mesh_file= '../../../data/deploy/data/Export_Analysis/9-LV SR Penta.mesh'
+path_model = './models'
+combine_models = False
+vtk_meta_text = 'PatientData S18 S18 4290_S18'
+fname_out = '../../../data/deploy/data/predictions_mapped_NoScar.vtk'
+path_data_parquet =  "../../../data/deploy/data/preprocessed_rawsignal_unipolar_penta.parquet"
 
 catheter_type = "Penta"
 
-
+models = [
+        "clf_NoScar_RVp_120epochs.pkl", 
+        "clf_NoScar_LVp_120epochs.pkl", 
+        "clf_NoScar_SR_120epochs.pkl",
+        "clf_AtLeastEndo_RVp_120epochs.pkl",
+        "clf_AtLeastEndo_LVp_120epochs.pkl",
+        "clf_AtLeastEndo_SR_120epochs.pkl",
+        "clf_AtLeastIntra_RVp_120epochs.pkl",
+        "clf_AtLeastIntra_LVp_120epochs.pkl",
+        "clf_AtLeastIntra_SR_120epochs.pkl",
+        "clf_epiOnly_RVp_120epochs.pkl",
+        "clf_epiOnly_LVp_120epochs.pkl",
+        "clf_epiOnly_SR_120epochs.pkl"
+    ]
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -111,7 +128,7 @@ def classify_ecg(path_data, path_models, outname_parquet):
     
     Example: 
         
-    path_data =  "../../../data/deploy/data/preprocessed_rawsignal_unipolar_penta.parquet"
+    path_data =  "../../../data/deploy/data/S18_RVp_NoScar_groundtruth.parquet"
     path_models = [
         "./models/clf_NoScar_RVp_120epochs.pkl", 
         "./models/clf_NoScar_LVp_120epochs.pkl", 
@@ -127,12 +144,11 @@ def classify_ecg(path_data, path_models, outname_parquet):
      # get coordinates of points
     dfcoord = pd.read_parquet(path_data, columns=['Point_Number','WaveFront','X','Y','Z'])
 
-    tsai = TSai('','') 
+    tsai = TSai('','')
     dfres = pd.DataFrame()
     for path_model in path_models:
         df = tsai.predict_from_file(path_data, path_model)
         dfres = pd.concat([dfres, df], axis=0)
-
     # merge predictions with coordinates on Point_Number and Wavefront
     dfres = dfres.merge(dfcoord, on=['Point_Number','WaveFront'], how='left')
     dfres.to_parquet(outname_parquet, index=False)
@@ -150,9 +166,63 @@ def postprocess_data(path_data_export, point_data_file, meshfile, fname_out_vtk,
     mapper = MeshDataMapper(path_data_export, point_data_file, meshfile, fname_out_vtk, meta_text)
     mapper.run()
 
+
+def run(data_dir, 
+                  path_model, 
+                  models, 
+                  meshfile, 
+                  path_out, 
+                  meta_text, 
+                  fname_preprocessed = None, 
+                  combine_models = False):
+    # check that data_dir, path_model, and meshfile exist
+    if not os.path.exists(data_dir):
+        raise FileNotFoundError(f"Data directory {data_dir} not found.")
+    if not os.path.exists(path_model):
+        raise FileNotFoundError(f"Model directory {path_model} not found.")
+    if not os.path.exists(meshfile):
+        raise FileNotFoundError(f"Mesh file {meshfile} not found.")
+    # check that models exist
+    path_models = [os.path.join(path_model, m) for m in models]
+    for path_model in path_models:
+        if not os.path.exists(path_model):
+            raise FileNotFoundError(f"Model file {path_model} not found.")
+
+    if not os.path.exists(path_out):
+        os.makedirs(path_out, exist_ok=True)
+
+    # preprocess data if needed
+    if not fname_preprocessed:
+        fname_preprocessed = preprocess_data(data_dir)
+
+    # classify ECG data for each model
+    if combine_models:
+        print("Generating combined model ...")
+        outname_parquet = os.path.join(path, f"predictions_combined.parquet")
+        classify_ecg(fname_preprocessed, path_model, outname_parquet)
+        fname_out_vtk = os.path.join(path_out, f"predictions_combined.vtk")
+        postprocess_data(data_dir, outname_parquet, meshfile, fname_out_vtk, meta_text)
+    else:
+        for path_model in path_models:
+            print(f"Processing model {os.path.basename(path_model)} ...")
+            # get path name  from path_models
+            path = os.path.dirname(path_model)
+            # get basename w/o .pkl
+            basename = os.path.basename(path_model).split('.')[0]
+            outname_parquet = os.path.join(path, f"predictions_{basename}.parquet")
+            classify_ecg(fname_preprocessed, path_model, outname_parquet)
+            # Postprocess data
+            fname_out_vtk = os.path.join(path_out, f"predictions_{basename}.vtk")
+            postprocess_data(data_dir, outname_parquet, meshfile, fname_out_vtk, meta_text)
+    print("Inference completed.")
+
+
+    
 def main():
     # Preprocess data
-    fname_preprocessed = preprocess_data(data_dir)
+    fname_preprocessed = preprocess_data(raw_data_path)
+
+    
 
     # Classify ECG data
     for path_model in path_models:
@@ -161,9 +231,10 @@ def main():
         # get basename w/o .pkl
         basename = os.path.basename(path_model).split('.')[0]
         outname_parquet = os.path.join(path, f"predictions_{basename}.parquet")
-        classify_ecg(fname_preprocessed, path_model, outname_parquet)
-        # Postprocess data
-        postprocess_data(data_dir, outname_parquet, meshfile, fname_out_vtk, meta_text)
+        if not combine_models:
+            classify_ecg(fname_preprocessed, path_model, outname_parquet)
+            # Postprocess data
+            postprocess_data(data_dir, outname_parquet, meshfile, fname_out_vtk, meta_text)
     
 
 
